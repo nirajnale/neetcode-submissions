6083cfaf216258804fbea2bc18c9ade8169de79a
nirajnale <132734839+nirajnale@users.noreply.github.com>
2026-06-16 22:19:52 +0530

    Add: longest-palindrome - submission-0

---
+++ Data Structures & Algorithms/longest-palindrome/submission-0.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+
+        freq = Counter(s)
+
+        length = 0
+        hasOdd = False
+
+        for count in freq.values():
+
+            if count % 2 == 0:
+                length += count
+            else:
+                length += count - 1
+                hasOdd = True
+
+        if hasOdd:
+            length += 1
+
+        return length

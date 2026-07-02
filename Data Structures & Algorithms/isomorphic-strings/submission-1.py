e6ab89046df545f089cac6b2a5cc4776eab314e0
nirajnale <132734839+nirajnale@users.noreply.github.com>
2026-07-02 23:28:13 +0530

    Add: isomorphic-strings - submission-1

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+
+        s_to_t = {}
+        t_to_s = {}
+
+        for c1, c2 in zip(s, t):
+
+            # existing mapping check
+            if c1 in s_to_t and s_to_t[c1] != c2:
+                return False
+
+            if c2 in t_to_s and t_to_s[c2] != c1:
+                return False
+
+            # create mapping
+            s_to_t[c1] = c2
+            t_to_s[c2] = c1
+
+        return True

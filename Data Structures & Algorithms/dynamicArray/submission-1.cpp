bc8790d3bf5213653b0769203c4c5efea619f722
Niraj Nale <132734839+nirajnale@users.noreply.github.com>
2026-08-15 20:05:20 +0530

    Add: dynamicArray - submission-1

---
+++ Data Structures & Algorithms/dynamicArray/submission-1.cpp
@@ -0,0 +1,67 @@
+class DynamicArray {
+private:
+    int* arr;
+    int size;
+    int capacity;
+
+public:
+    DynamicArray(int capacity) {
+        this->capacity = capacity;
+        this->size = 0;
+        this->arr = new int[capacity];
+    }
+
+    int get(int i) {
+        return arr[i];
+    }
+
+    void set(int i, int n) {
+        arr[i] = n;
+    }
+
+    void pushback(int n) {
+
+        // Array is full
+        if (size == capacity) {
+            resize();
+        }
+
+        arr[size] = n;
+        size++;
+    }
+
+    int popback() {
+
+        size--;
+        return arr[size];
+    }
+
+    void resize() {
+
+        int newCapacity = capacity * 2;
+
+        int* newArr = new int[newCapacity];
+
+        for (int i = 0; i < size; i++) {
+            newArr[i] = arr[i];
+        }
+
+        delete[] arr;
+
+        arr = newArr;
+        capacity = newCapacity;
+    }
+
+    int getSize() {
+        return size;
+    }
+
+    int getCapacity() {
+        return capacity;
+    }
+
+    // Good practice: release dynamically allocated memory
+    ~DynamicArray() {
+        delete[] arr;
+    }
+};

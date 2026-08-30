c5495f8e9749e4ac12b632c61c8624c1f29acf8e
Niraj Nale <132734839+nirajnale@users.noreply.github.com>
2026-08-30 23:17:18 +0530

    Add: take-gifts-from-the-richest-pile - submission-0

---
+++ Data Structures & Algorithms/take-gifts-from-the-richest-pile/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+
+        priority_queue<int> pq;
+
+        // Put all piles into max heap
+        for (int gift : gifts) {
+            pq.push(gift);
+        }
+
+        // Perform k operations
+        while (k--) {
+
+            int largest = pq.top();
+            pq.pop();
+
+            int remaining = sqrt(largest);
+
+            pq.push(remaining);
+        }
+
+        // Calculate total
+        long long ans = 0;
+
+        while (!pq.empty()) {
+            ans += pq.top();
+            pq.pop();
+        }
+
+        return ans;
+    }
+};

4c7fb5f602a60decb3759a12a7b6563a9a429566
Niraj Nale <132734839+nirajnale@users.noreply.github.com>
2026-09-09 23:30:53 +0530

    Add: sort-the-people - submission-1

---
+++ Data Structures & Algorithms/sort-the-people/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        vector<pair<int, string>> people;
+
+        for (int i = 0; i < names.size(); i++) {
+            people.push_back({heights[i], names[i]});
+        }
+
+        sort(people.begin(), people.end(), greater<pair<int, string>>());
+
+        vector<string> result;
+
+        for (auto &p : people) {
+            result.push_back(p.second);
+        }
+
+        return result;
+    }
+};

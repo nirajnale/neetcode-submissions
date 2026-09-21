42e6328a6a9d8a560ebc7eb6d310714bfae4717d
Niraj Nale <132734839+nirajnale@users.noreply.github.com>
2026-09-21 23:53:49 +0530

    Add: counting-words-with-a-given-prefix - submission-2

---
+++ Data Structures & Algorithms/counting-words-with-a-given-prefix/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int count = 0;
+
+        for (string word : words) {
+            if (word.size() >= pref.size() &&
+                word.compare(0, pref.size(), pref) == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
